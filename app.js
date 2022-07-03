071abd010ae2d2e1b51f3c0b944ce52b85b89a6b
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-07-03 07:46:05 +0200

    fetching the quotes

--- app.js
+++ app.js
@@ -6,3 +6,15 @@ const speechBtn = document.querySelector(".sound")
 const copyBtn = document.querySelector(".copy")
 const twitterBtn = document.querySelector(".twitter")
 const synth = document.querySelector(".sound")
+const API_URL = "https://animechan.vercel.app/api/random"
+
+
+function randomQuote() {
+    fetch(API_URL)
+        .then(response => response.json())
+        .then(result => {
+            console.log(result)
+        });
+}
+
+randomQuote()
